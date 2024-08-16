4ecca00972881685a39a90727c04d3710a1005af
Javier Rivas <ra13037@ues.edu.sv>
2024-08-15 18:23:02 -0600

    Modificado Terminado

--- cinestar.py
+++ cinestar.py
@@ -1,65 +1,65 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from datetime import date
+from datetime import date, timedelta # Importar timedelta para agregar el dia siguiente a la fecha
 import pandas as pd
 
-browser = webdriver.Firefox()
+browser = webdriver.Firefox() # Inicializa el navegador Firefox utilizando Selenium
 
-URL = 'https://cinestar.com.gt/'
+URL = 'https://cinestar.com.gt/' # URL del sitio web de Cinestar
 
-browser.get(URL)
+browser.get(URL) # Navega a la URL especificada
 
-peli = []
+peli = [] # Lista para almacenar la información de las películas
+
+# Función que agrega los datos de cada película a la lista 'peli'
 def fun(fecha, pais, cine, nombre_cine, titulo, idioma, formato, horarios):
   for h in horarios:
     datos = {
-      "Fecha": fecha,
-      "País": pais,
-      "Cine": cine,
-      "Nombre de cine": nombre_cine,
-      "Titulo": titulo,
-      "Idioma": idioma,
-      "Formato": formato,
-      "Hora": h.text
+      "Fecha": fecha,                 # Fecha de la función de la película
+      "País": pais,                   # País donde se encuentra el cine
+      "Cine": cine,                   # Cadena de cines (en este caso 'Caribbean Cinemas')
+      "Nombre de cine": nombre_cine,  # Nombre específico del cine
+      "Titulo": titulo,               # Título de la película
+      "Idioma": idioma,               # Idioma en que se proyecta la película
+      "Formato": formato,             # Formato de la proyección (2D, 3D, etc.)
+      "Hora": h.text                  # Horario de la función
     }
-    peli.append(datos)
+    peli.append(datos) # Agrega los datos de la película a la lista 'peli'
 
+# Encuentra los enlaces a los diferentes cines dentro de la página
 teatros = browser.find_elements(By.XPATH, '//ul[@id="menu-main-menu"]//li[5]//ul[@class="sub-menu "]/li/a')
-lista1 = [link.get_attribute('href') for link in teatros]
+lista1 = [link.get_attribute('href') for link in teatros] # Obtiene la URL de cada cine
 
-for i in lista1:
-    browser.get(i)
-    fecha = date.today()
+for i in lista1:         # Itera sobre la lista de URLs de los cines
+    browser.get(i)       # Navega a la URL del cine actual
+    fecha = date.today() # Navega fecha actual # Modificación: fecha del día siguiente agregar + timedelta(days=1)
     pais = 'Guatemala'
     cine = 'Caribbean Cinemas'
-    nombre_cine = browser.find_element(By.XPATH, '//div[@id="cineinfo"]//b').text 
+    nombre_cine = browser.find_element(By.XPATH, '//div[@id="cineinfo"]//b').text # Nombre del cine
     
+    # Encuentra todos los elementos que contienen las películas y sus horarios
     cartelera = browser.find_elements(By.XPATH, '//div[@id="horarios"]/*')
-    print(len(cartelera))
-
+    
+    # Itera sobre cada película en la cartelera
     for movie in cartelera:
-        titulo = movie.find_element(By.XPATH, './/b')
-        idioma_formato = movie.find_element(By.XPATH, './/i')
-        idioma_formato = idioma_formato.text.split(' ')
+        titulo = movie.find_element(By.XPATH, './/b')         # Encuentra el título de la película
+        idioma_formato = movie.find_element(By.XPATH, './/i') # Encuentra el idioma y formato
+        idioma_formato = idioma_formato.text.split(' ')       # Separa el texto para obtener el idioma y el formato
         if len(idioma_formato)>1:
-            formato = idioma_formato[0]
-            idioma = idioma_formato[1]
+            formato = idioma_formato[0] # Si hay formato, lo asigna
+            idioma = idioma_formato[1]  # Asigna el idioma
         else :
-            idioma = idioma_formato[0] 
-            formato = '2D'
-        horario = movie.find_elements(By.XPATH, './/div [@class="column three-fourth"]//div[2]/a')
-        
-        fun(fecha, pais, cine, nombre_cine, titulo.text, idioma, formato, horario)
+            idioma = idioma_formato[0]  # Si solo hay un elemento, asume que es el idioma
+            formato = '2D'              # Asume que el formato es '2D' por defecto
+        horario = movie.find_elements(By.XPATH, './/div [@class="column three-fourth"]//div[2]/a') # Encuentra los horarios
+        fun(fecha, pais, cine, nombre_cine, titulo.text, idioma, formato, horario) # Llama a la función para agregar los dato
                            
 
-
-
-
- 		  
-           
-        
-
+	  
+   
+# Cierra el navegador       
 browser.quit()
 
+# Crea un DataFrame con los datos recopilados y lo exporta a un archivo Excel
 df = pd.DataFrame(peli)
-df.to_excel('horarios_cinestar.xlsx', index = False)
+df.to_excel('Cinestar - '+str(date.today())+'.xlsx', index = False) # Guarda el DataFrame en un archivo Excel
--- novacinemacr.py
+++ novacinemacr.py
@@ -5,94 +5,125 @@ import pandas as pd
 from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
-browser = webdriver.Firefox()
+browser = webdriver.Firefox()        # Inicia el navegador Firefox
 
-URL = 'https://www.novacinemas.cr/'
+URL = 'https://www.novacinemas.cr/'  # Establece la URL a visitar
 
-browser.get(URL)
+browser.get(URL)                     # Abre la página de Novacinemas
 
-horarios_pelicula = []
+horarios_pelicula = []               # Lista para almacenar la información de los horarios de las películas
+
+# Función para agregar los datos de la película a la lista
 def agregar_datos(nombre_cine, titulo, idioma, formato, hora):
   
     datos = {
-      "Fecha": date.today(),
-      "País": "Costa Rica",
-      "Cine": "Novacinemas",
-      "Nombre del cine": nombre_cine,
-      "Titulo": titulo,
-      "Idioma": idioma,
-      "Formato": formato,
-      "Hora": hora
+      "Fecha": date.today(), # Almacena la fecha actual (+ timedelta(days=1),  # Modificación: obtener la fecha del día siguiente)
+      "País": "Costa Rica",             # Establece el país como Costa Rica
+      "Cine": "Novacinemas",            # Establece el nombre de la cadena de cines
+      "Nombre del cine": nombre_cine,   # Nombre del cine específico
+      "Titulo": titulo,                 # Título de la película
+      "Idioma": idioma,                 # Idioma de la película
+      "Formato": formato,               # Formato de la película (2D, 3D, etc.)
+      "Hora": hora                      # Hora de la función
 
     }
 
-    horarios_pelicula.append(datos)
+    horarios_pelicula.append(datos)     # Agrega el diccionario con los datos a la lista
 
-#Busca Cartelera y hace click
-cartelera_link = browser.find_element(By.XPATH, '//ul[@id="menu-menu"]//li[1]/a')
-sleep(1)
-cartelera_link.click()
-
-
-for i in range(1,3):
-        
-    cartelera = browser.find_element(By.XPATH, '//div[@id="cartelera"]')
-    sleep(3)
-    select_cine = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable(cartelera.find_element(By.XPATH, './/div[@id="selectCinema"]/span'))
+# Encuentra el enlace de la cartelera y hace clic en él
+try:
+    cartelera_link = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//ul[@id="menu-menu"]//li[1]/a'))
     )
-    select_cine.click()
-    #cine = cartelera.find_element(By.XPATH, './/div[@class="topSelect selector-cine"]//div[@id="selectCinema"]//div[@class="sel-box"]/ul/li//a['+str(i)+']')
-    cine = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable(cartelera.find_element(By.XPATH, './/div[@id="selectCinema"]//div[@class="sel-box"]/ul/li['+str(i)+']/a'))
-        )
-    id_cine = cine.get_attribute('data-cinema-id')
-    
-    cine.click()
+    cartelera_link.click()
+except TimeoutException:
+    print("Error: No se pudo encontrar el enlace de la cartelera.")
+    browser.quit()
+    exit()  
 
-    select_fecha = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable(cartelera.find_element(By.XPATH, './/div[@class="topSelect selector-fecha"]/nav/a'))
-    )
-    select_fecha.click()
-    #cine = cartelera.find_element(By.XPATH, './/div[@class="topSelect selector-cine"]//div[@id="selectCinema"]//div[@class="sel-box"]/ul/li//a['+str(i)+']')
-    fecha = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable(cartelera.find_element(By.XPATH, './/div[@class="topSelect selector-fecha"]/nav/ul/li[2]/a'))
-        )
-    fecha.click()
-    
-    divs = browser.find_elements(By.XPATH, '//div[@id="cinema-'+id_cine+'"][2]/ul/*')
-    nombre_cine = browser.find_element(By.XPATH, '//div[@id="cinema-'+id_cine+'"][2]/h2').text 
-    
-    for div in divs:
-        
-        if div.is_displayed():
-            peliculas = div.find_elements(By.XPATH, './li//div[@class="cols movieDates"]')
+try:
+# Itera sobre los cines disponibles (ajusta el rango según el número de cines)
+    for i in range(1,4): # Cambia el rango a `range(1, n)` donde n es el número total de cines
             
-            print(len(peliculas))
-            for pelicula in peliculas:
-                titulo = pelicula.find_element(By.XPATH, './/div[@class="col movieDates__right"]/div/h3').text
-                horarios = pelicula.find_elements(By.XPATH, './/div[@class="showTimes"]//div[@class="items-list"]/*')
-                for horario in horarios:
-                   idioma_formato = horario.find_element(By.XPATH, './span').text
-                   print(idioma_formato.split(' '))
-                   formato, idioma = idioma_formato.split(" ")
-                   
-                   hora = horario.find_element(By.XPATH, './a').text
-                   agregar_datos( nombre_cine, titulo, idioma, formato, hora)
+        cartelera = browser.find_element(By.XPATH, '//div[@id="cartelera"]') # Encuentra la sección de cartelera
+        sleep(3)        # Espera 3 segundos para que la cartelera se cargue completamente
+        
+        # Espera hasta que el menú desplegable de cines sea clicable y luego hace clic
+        select_cine = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable(cartelera.find_element(By.XPATH, './/div[@id="selectCinema"]/span'))
+        )
+        select_cine.click()
+        # Encuentra y selecciona el cine correspondiente basado en la iteración
+        cine = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable(cartelera.find_element(By.XPATH, './/div[@id="selectCinema"]//div[@class="sel-box"]/ul/li['+str(i)+']/a'))
+            )
+        id_cine = cine.get_attribute('data-cinema-id')  # Obtiene el ID del cine seleccionado
+        cine.click()                                    # Hace clic para seleccionar el cine
+
+        # Espera hasta que el menú de fechas sea clicable y luego hace clic
+        select_fecha = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable(cartelera.find_element(By.XPATH, './/div[@class="topSelect selector-fecha"]/nav/a'))
+        )
+        select_fecha.click()
+        
+        # Encuentra y selecciona la fecha para la cartelera 
+        fecha = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable(cartelera.find_element(By.XPATH, './/div[@class="topSelect selector-fecha"]/nav/ul/li[2]/a'))
+            )           #Cambiado a li[3] para obtener el segundo día
+        fecha.click()
+        
+        # Encuentra todas las películas para el cine y la fecha seleccionados
+        divs = browser.find_elements(By.XPATH, '//div[@id="cinema-'+id_cine+'"][2]/ul/*')
+        nombre_cine = browser.find_element(By.XPATH, '//div[@id="cinema-'+id_cine+'"][2]/h2').text  # Obtiene el nombre del cine
+        
+        for div in divs:            # Recorre cada película disponible en la cartelera
+            
+            if div.is_displayed():  # Verifica que la película esté visible
+                # Encuentra todas las películas en la lista
+                peliculas = div.find_elements(By.XPATH, './li//div[@class="cols movieDates"]') 
                 
+                #print(len(peliculas))
+                for pelicula in peliculas:
+
+                    # Obtiene el título de la película
+                    titulo = pelicula.find_element(By.XPATH, './/div[@class="col movieDates__right"]/div/h3').text
+
+                    # Encuentra los horarios de la película
+                    horarios = pelicula.find_elements(By.XPATH, './/div[@class="showTimes"]//div[@class="items-list"]/*')
+
+                    for horario in horarios:                                            # Recorre cada horario encontrado
+                        idioma_formato = horario.find_element(By.XPATH, './span').text   # Obtiene el formato e idioma del horario
+                        partes = idioma_formato.split(" ")
+
+                        if len(partes) == 2:
+                                formato, idioma = partes
+                        else:
+                            # En caso de que no se pueda separar en dos partes, asignar valores predeterminados o continuar
+                            formato = partes[0] if len(partes) > 0 else "Desconocido"
+                            idioma = "Desconocido"
+
+                        hora = horario.find_element(By.XPATH, './a').text    # Obtiene la hora de la función
+
+                        # Agrega los datos obtenidos a la lista
+                        agregar_datos( nombre_cine, titulo, idioma, formato, hora)
+except TimeoutException:
+    print("Error: No se pudo encontrar el enlace del cine.")
+    browser.quit()
+    exit()                
             
                    
                  
                 
-                
-                
-
-                
-
+              
+               
+# Cierra el navegador           
 browser.quit()
 
+# Crea un DataFrame a partir de la lista de horarios de películas
 df = pd.DataFrame(horarios_pelicula)
+# Guarda los datos en un archivo Excel con la fecha actual en el nombre
 df.to_excel('Novacinemas - '+str(date.today())+'.xlsx', index = False)
 
             
