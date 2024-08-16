3341de18091cb2d91e3ae2e26d79e0378743c841
contreras48 <contreras4893@gmail.com>
2024-08-16 17:57:28 -0600

    Se agregaron archivos run.bat y run.sh Estos archivos ayudarn a escutar todos los scripts  sin tenerlos que ejecutar uno a uno

---
+++ run.bat
@@ -0,0 +1,16 @@
+@echo off
+python 1_ccm_cinemas.py
+python 2_novacinemacr.py
+python 3_multicinema.py
+python 4_cinestar.py
+python 5_metrocinema.py
+python 6_unicines.py
+python 7_siglo_nuevo.py
+python 8_caribbean_cinemas_panama.py
+python 9_cinesmoderno.py
+python 10_cinepolis_honduras.py
+python 11_cinepolis_guatemala.py
+python 12_cinepolis_el_salvador.py
+python 13_cinepolis_costa_rica.py
+python 14_cinepolis_panama.py
+pause
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+python3 1_ccm_cinemas.py
+python3 2_novacinemacr.py
+python3 3_multicinema.py
+python3 4_cinestar.py
+python3 5_metrocinema.py
+python3 6_unicines.py
+python3 7_siglo_nuevo.py
+python3 8_caribbean_cinemas_panama.py
+python3 9_cinesmoderno.py
+python3 10_cinepolis_honduras.py
+python3 11_cinepolis_guatemala.py
+python3 12_cinepolis_el_salvador.py
+python3 13_cinepolis_costa_rica.py
+python3 14_cinepolis_panama.py
